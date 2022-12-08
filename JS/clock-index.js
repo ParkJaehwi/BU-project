e6b07228c9f4d6cd78149751014cbce4e60d27cf
Jay <84843445+ParkJaehwi@users.noreply.github.com>
2022-12-08 22:40:02 +0900

    Add files via upload

---
+++ JS/clock-index.js
@@ -0,0 +1,30 @@
+const clock = document.querySelector("#clock-index-time");
+const cal = document.querySelector("#cal");
+const monthEng = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const year = date.getFullYear();
+  const month = parseInt(("0" + date.getMonth() + 1).slice(-2));
+  const day = ("0" + date.getDate()).slice(-2);
+  console.log(month);
+  clock.innerText = ` ${hours} : ${minutes} `;
+  cal.innerText = ` ${year}. ${month}. ${day}`;
+}
+getClock();
+setInterval(getClock, 1000);
